40480f8eba1087735b21bdcac249b219b58a93f2
Paul Gribble <paul@gribblelab.org>
2016-12-09 17:22:22 -0500

    day9.2 more efforts

--- day9.2.c
+++ day9.2.c
@@ -2,7 +2,45 @@
 #include <stdlib.h>
 #include <string.h>
 
-// need to go backwards starting at end and expanding
+long int buflen(char *buf) {
+
+  long int bufall_len = (long int)strlen(buf);
+  char *cb1 = strchr(buf,'(');
+  if (cb1 != NULL) {
+    char *cb2 = strchr(buf,')');
+    int cbi1 = (int)(cb1-buf);
+    int cbi2 = (int)(cb2-buf);
+    char *cx = strchr(buf,'x');
+    int cxi = (int)(cx-buf);
+    char *m = malloc(sizeof(char)*(cbi2-cbi1+1));
+    for (int i=0; i<(cbi2-cbi1+1); i++) m[i]=buf[cbi1+i];
+    m[cbi2-cbi1+1]='\0';
+    m[cxi]='\0';
+    int m1 = atoi(m+sizeof(char));
+    int m2 = atoi(m+(sizeof(char)*cxi+1));
+    free(m);
+    char *mbuf = malloc((sizeof(char)*m1*m2) + 1);
+    mbuf[sizeof(char)*m1*m2] = '\0';
+    int mbufi = 0;
+    for (int i=0; i<m2; i++) {
+      for (int j=0; j<m1; j++) {
+	mbuf[mbufi] = buf[cbi2+1+j];
+	mbufi = mbufi + 1;
+      }
+    }
+    long int mbl = (long int)buflen(mbuf);
+    printf("%s|",mbuf);
+    free(mbuf);
+    char *rest = &(buf[cbi2+1+m1]);
+    printf("%s\n",rest);
+    return cbi1 + mbl + buflen(rest);
+  }
+  else {
+    long int blen = (long int)strlen(buf);
+    return blen;
+  }
+  
+}
 
 int main()
 {
@@ -12,7 +50,7 @@ int main()
   long int dflen = 0;
   
   fscanf(fid,"%s\n",buf);
-  dflen = strlen(buf);
+  dflen = buflen(buf);
   printf("%ld\n", dflen);
 
   fclose(fid);
