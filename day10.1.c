167c0712e0209e9c65c5b0852c364167e81c8876
Paul Gribble <paul@gribblelab.org>
2016-12-10 14:41:00 -0500

    day10 I misread instructions

--- day10.1.c
+++ day10.1.c
@@ -2,69 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+int bots[210][2];
+
+void showbots() {
+  printf("-------------------\n");  
+  for (int i=0; i<210; i++) {
+    printf("bot %4d: %4d %4d\n",i,bots[i][0],bots[i][1]);
+  }
+  printf("-------------------\n");  
+}
+
 int main(int argc, char *argv[]) {
 
-  char *buf = malloc(sizeof(char)*16);
-  FILE *fid = fopen("day10_input.txt","r");
-  int b1,blo,bhi,dummyint1,dummyint2;
-  int bots[256][2];
-  int val;
-  for (int i=0; i<256; i++) { bots[i][0] = -1; bots[i][1] = -1; }
-  char *dummy = malloc(sizeof(char)*64);
-  
-  while (fscanf(fid,"%s",buf) != EOF) {
-    if (strcmp(buf,"bot")==0) {
-      fscanf(fid,"%d",&b1);
-      fscanf(fid,"%s%s%s",dummy,dummy,dummy); // gives low to
-      fscanf(fid,"%s",buf);
-      if (strcmp(buf,"output")==0) {
-	bots[b1][0] = -1;
-	fscanf(fid,"%d",&dummyint1);
-	fscanf(fid,"%s%s%s",dummy,dummy,dummy); // and hi to
-	fscanf(fid,"%s",buf);
-	if (strcmp(buf,"output")==0) {
-	  bots[b1][1] = -1;
-	  fscanf(fid,"%d",&dummyint2);
-	  printf("bot %d gives low to output %d and high to output %d\n",b1,dummyint1,dummyint2);
-	}
-	else {
-	  fscanf(fid,"%d",&bhi);
-	  // give bots[b1] hi to bots[bhi]
-	  // (need to write code to do it here)
-	  printf("bot %d gives low to output %d and high to bot %d\n",b1,dummyint1,bhi);
-	}
-      }
-      else {
-	fscanf(fid,"%d",&blo);
-	// give bots[b1] lo to bots[blo]
-	// (need to write code to do it here)
-	printf("bot %d gives low to bot %d",b1,blo);
-	bots[b1][0] = -1;
-	fscanf(fid,"%s%s%s",dummy,dummy,dummy); // and high to
-	fscanf(fid,"%s",buf);
-	if (strcmp(buf,"output")==0) bots[b1][1] = -1;
-	else {
-	  fscanf(fid,"%d",&bhi);
-	  // give bots[b1] hi to bots[bhi]
-	  // (need to write code to do it here)
-	  printf(" and high to bot %d\n",bhi);
-	  bots[b1][1] = -1;
-	}
-      }
-    }
-    else {
-      fscanf(fid,"%d",&val);
-      fscanf(fid,"%s%s%s",dummy,dummy,dummy); // goes to bot
-      fscanf(fid,"%d",&b1);
-      // give bots[b1] value val
-      // (need to write code to do it here)
-      printf("value %d goes to bot %d\n",val,b1);
-    }    
-  }
+  for (int i=0; i<210; i++) for (int j=0; j<2; j++) bots[i][j] = -1;
 
-  free(dummy);
+  
+  FILE *fid = fopen("day10_input.txt","r");
+  
   fclose(fid);
-  free(buf);
+
+  
+  showbots();
   
   return 0;
 
