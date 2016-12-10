03c9ac8f5d6fb134a83c0be001a00aab2815e20c
Paul Gribble <paul@gribblelab.org>
2016-12-10 10:41:18 -0500

    more progress on day 10

--- day10.1.c
+++ day10.1.c
@@ -7,17 +7,49 @@ int main(int argc, char *argv[]) {
   char *buf = malloc(sizeof(char)*16);
   FILE *fid = fopen("day10_input.txt","r");
   int b1,blo,bhi;
-  int outputs[20][24];
-  for (int i=0; i<20; i++) for (int j=0; j<24; j++) outputs[i][j]=0;
-  int bots[256][24];
-  for (int i=0; i<256; i++) for (int j=0; j<24; j++) bots[i][j]=0;
+  int bots[256][2];
+  int val;
+  for (int i=0; i<256; i++) { bots[i][0] = -1; bots[i][1] = -1; }
+  char *dummy = malloc(sizeof(char)*64);
   
   while (fscanf(fid,"%s",buf) != EOF) {
     if (strcmp(buf,"bot")==0) {
-      
+      fscanf(fid,"%d",&b1);
+      fscanf(fid,"%s %s %s",dummy,dummy,dummy); // gives low to
+      fscanf(fid,"%s",buf);
+      if (strcmp(buf,"output")==0) bots[b1][0] = -1;
+      else {
+	fscanf(fid,"%d",&blo);
+	// give bots[b1] lo to bots[blo]
+	// (need to write code to do it here)
+
+	printf("bot %d gives low to bot %d",b1,blo);
+	bots[b1][0] = -1;
+	fscanf(fid,"%s %s %s",dummy,dummy,dummy); // and high to
+	fscanf(fid,"%s",buf);
+	if (strcmp(buf,"output")==0) bots[b1][1] = -1;
+	else {
+	  fscanf(fid,"%d",&bhi);
+	  // give bots[b1] hi to bots[bhi]
+	  // (need to write code to do it here)
+	  printf(" and high to bot %d\n",bhi);
+
+	  bots[b1][1] = -1;
+	}
+      }
     }
+    else {
+      fscanf(fid,"%d",&val);
+      fscanf(fid,"%s %s %s",dummy,dummy,dummy); // goes to bot
+      fscanf(fid,"%d",&b1);
+      // give bots[b1] value val
+      // (need to write code to do it here)
+
+      printf("value %d goes to bot %d\n",val,b1);
+    }    
   }
-  
+
+  free(dummy);
   fclose(fid);
   free(buf);
   
