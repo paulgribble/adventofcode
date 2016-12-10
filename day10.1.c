30f9e3f69789a13c5135e2728932b67fc759b348
Paul Gribble <paul@gribblelab.org>
2016-12-10 13:55:18 -0500

    day10 progress

--- day10.1.c
+++ day10.1.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]) {
 
   char *buf = malloc(sizeof(char)*16);
   FILE *fid = fopen("day10_input.txt","r");
-  int b1,blo,bhi;
+  int b1,blo,bhi,dummyint1,dummyint2;
   int bots[256][2];
   int val;
   for (int i=0; i<256; i++) { bots[i][0] = -1; bots[i][1] = -1; }
@@ -15,9 +15,25 @@ int main(int argc, char *argv[]) {
   while (fscanf(fid,"%s",buf) != EOF) {
     if (strcmp(buf,"bot")==0) {
       fscanf(fid,"%d",&b1);
-      fscanf(fid,"%s %s %s",dummy,dummy,dummy); // gives low to
+      fscanf(fid,"%s%s%s",dummy,dummy,dummy); // gives low to
       fscanf(fid,"%s",buf);
-      if (strcmp(buf,"output")==0) bots[b1][0] = -1;
+      if (strcmp(buf,"output")==0) {
+	bots[b1][0] = -1;
+	fscanf(fid,"%d",&dummyint1);
+	fscanf(fid,"%s%s%s",dummy,dummy,dummy); // and hi to
+	fscanf(fid,"%s",buf);
+	if (strcmp(buf,"output")==0) {
+	  bots[b1][1] = -1;
+	  fscanf(fid,"%d",&dummyint2);
+	  printf("bot %d gives low to output %d and high to output %d\n",b1,dummyint1,dummyint2);
+	}
+	else {
+	  fscanf(fid,"%d",&bhi);
+	  // give bots[b1] hi to bots[bhi]
+	  // (need to write code to do it here)
+	  printf("bot %d gives low to output %d and high to bot %d\n",b1,dummyint1,bhi);
+	}
+      }
       else {
 	fscanf(fid,"%d",&blo);
 	// give bots[b1] lo to bots[blo]
@@ -25,7 +41,7 @@ int main(int argc, char *argv[]) {
 
 	printf("bot %d gives low to bot %d",b1,blo);
 	bots[b1][0] = -1;
-	fscanf(fid,"%s %s %s",dummy,dummy,dummy); // and high to
+	fscanf(fid,"%s%s%s",dummy,dummy,dummy); // and high to
 	fscanf(fid,"%s",buf);
 	if (strcmp(buf,"output")==0) bots[b1][1] = -1;
 	else {
@@ -40,7 +56,7 @@ int main(int argc, char *argv[]) {
     }
     else {
       fscanf(fid,"%d",&val);
-      fscanf(fid,"%s %s %s",dummy,dummy,dummy); // goes to bot
+      fscanf(fid,"%s%s%s",dummy,dummy,dummy); // goes to bot
       fscanf(fid,"%d",&b1);
       // give bots[b1] value val
       // (need to write code to do it here)
