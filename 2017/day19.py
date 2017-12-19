9cc7ed2e7ec5b2e08d83c4563f559ce91c28ed42
Paul Gribble <paul@gribblelab.org>
2017-12-19 12:08:47 -0500

    cleanup

--- 2017/day19.py
+++ 2017/day19.py
@@ -1,42 +1,41 @@
 
-
 grid = [l.strip('\n') for l in open('day19_input.txt')]
 
 x,y = 39,0 # entry point into the grid
 
-heading = 'S'
+heading = 'south'
 letters = []
-tile = '|'
+tile = grid[y][x]
 steps = 0
 done = False
 while (done==False):
 	steps += 1
-	if (heading=='S'):
+	if (heading=='south'):
 		y += 1
-	elif (heading=='N'):
+	elif (heading=='north'):
 		y -= 1
-	elif (heading=='W'):
+	elif (heading=='west'):
 		x -= 1
-	elif (heading=='E'):
+	elif (heading=='east'):
 		x += 1
 	tile = grid[y][x] # be careful: y (north/south) is rows, x is cols
 	if (tile == ' '): # we're done
 		done = True
 	elif (tile in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
-		letters += tile # add letter and keep on same heading
+		letters += tile # add letter to list and keep on same heading
 	elif (tile=='+'):
-		if ((heading=='N') | (heading=='S')):
+		if ((heading=='north') | (heading=='south')):
 			look_west = grid[y][x-1]
 			if (look_west==' '): # don't go that way
-				heading = 'E'
+				heading = 'east'
 			else:
-				heading = 'W'
+				heading = 'west'
 		else: # heading is E or W
 			look_north = grid[y-1][x]
 			if (look_north==' '): # don't go that way
-				heading = 'S'
+				heading = 'south'
 			else:
-				heading = 'N'
+				heading = 'north'
 
 print('part 1: we will see the letters {}'.format(''.join(letters)))
 print('part 2: we will take {} steps'.format(steps))
