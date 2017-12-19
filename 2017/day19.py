5179eb6564e5c258301ac5be6b5f26177fad5b1a
Paul Gribble <paul@gribblelab.org>
2017-12-19 09:42:26 -0500

    cleanup

--- 2017/day19.py
+++ 2017/day19.py
@@ -2,7 +2,7 @@
 
 grid = [l.strip('\n') for l in open('day19_input.txt')]
 
-x, y = 39, 0 # entry point into the grid
+x,y = 39,0 # entry point into the grid
 
 heading = 'S'
 letters = []
@@ -19,23 +19,24 @@ while (done==False):
 		x -= 1
 	elif (heading=='E'):
 		x += 1
-	tile = grid[y][x]
-	if (tile == ' '):
+	tile = grid[y][x] # be careful: y (north/south) is rows, x is cols
+	if (tile == ' '): # we're done
 		done = True
-	else:
-		if (tile=='+'):
-			if ((heading=='N') | (heading=='S')):
-				if (grid[y][x-1] != ' '):
-					heading = 'W'
-				else:
-					heading = 'E'
+	elif (tile in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
+		letters += tile # add letter and keep on same heading
+	elif (tile=='+'):
+		if ((heading=='N') | (heading=='S')):
+			look_west = grid[y][x-1]
+			if (look_west==' '): # don't go that way
+				heading = 'E'
 			else:
-				if (grid[y-1][x] != ' '):
-					heading = 'N'
-				else:
-					heading = 'S'
-		elif (tile in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
-			letters += tile
+				heading = 'W'
+		else: # heading is E or W
+			look_north = grid[y-1][x]
+			if (look_north==' '): # don't go that way
+				heading = 'S'
+			else:
+				heading = 'N'
 
 print('part 1: we will see the letters {}'.format(''.join(letters)))
 print('part 2: we will take {} steps'.format(steps))
