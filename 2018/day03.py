bbb5a9c97d0c241651fd8b54c42ccb7e54f9c445
Paul Gribble <paul@gribblelab.org>
2018-12-03 07:48:25 -0500

    part 2 done

--- 2018/day03.py
+++ 2018/day03.py
@@ -2,11 +2,14 @@
 
 import numpy as np 
 
+# Part 1
+
 nn = 1280
 fabric = np.zeros(shape=(nn,nn))
+
 f = open("day03_input.txt","r")
 for line in f:
-	n,ll = line.strip('\n').split(" @ ")
+	ID,ll = line.strip('\n').split(" @ ")
 	corner, size = ll.split(": ")
 	cc,cr = corner.split(",")
 	sc,sr = size.split("x")
@@ -15,4 +18,17 @@ for line in f:
 f.close()
 num = sum(sum(fabric>=2))
 print("Day 3 Part 1: {:d} sq inches of fabric are within two or more claims".format(num))
+
+# Part 2
+f = open("day03_input.txt","r")
+for line in f:
+	ID,ll = line.strip('\n').split(" @ ")
+	corner, size = ll.split(": ")
+	cc,cr = corner.split(",")
+	sc,sr = size.split("x")
+	cc,cr,sc,sr = int(cc),int(cr),int(sc),int(sr)
+	patch = fabric[cr:cr+sr,cc:cc+sc]
+	if sum(sum(patch != 1)) == 0:
+		print("Day 3 Part 2: {:s} is the ID of the claim that doesn't overlap".format(ID))
+f.close()
 
