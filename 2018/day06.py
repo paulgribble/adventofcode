692621e857a9679463c25e2578e5e0aa413384ae
Paul Gribble <paul@gribblelab.org>
2018-12-06 12:05:37 -0500

    day06 done

--- 2018/day06.py
+++ 2018/day06.py
@@ -4,20 +4,19 @@
 
 import numpy as np
 
-#points = np.loadtxt("day06_test_input.txt", delimiter=',')
 points = np.loadtxt("day06_input.txt", delimiter=',')
-points_rc = points[:,[1,0]]
+#points = np.loadtxt("day06_input.txt", delimiter=',')
 n = np.shape(points)[0]
 
 max_x = np.max(points[:,0])
 max_y = np.max(points[:,1])
-n_c, n_r = int(max_x)+1, int(max_y)+1
+n_c, n_r = int(max_x), int(max_y)
 
 grid = np.ones(shape=(n_r,n_c)) * -1 # -1 is code for baaaad
 
 for c in range(n_c):
 	for r in range(n_r):
-		d = np.sum(abs(points_rc - np.array([r,c])), 1) # manhattan
+		d = np.sum(abs(points - np.array([r,c])), 1) # manhattan
 		p_closest = np.argmin(d) # which point is the closest
 		if (np.sum(d==d[p_closest]) == 1): # not a tie
 			grid[r,c] = p_closest
@@ -41,6 +40,17 @@ for i in range(n):
 
 print("Day 6 Part 1: the size of the largest area that isn't infinite is {:d}".format(max_area))
 
+# Part Two
 
+region = 0
+grid2 = np.copy(grid)
+for c in range(n_c):
+	for r in range(n_r):
+#		if (grid[r,c] > -1): # safe points only
+		d = np.sum(abs(points - np.array([r,c])))
+		grid2[r,c] = d
+		if (d < 10000):
+			region = region + 1
 
+print("Day 6 Part 2: the size of the region containing all locations which have a total distance to all given coordinatesof less than 10000 is {:d}".format(region))
 
