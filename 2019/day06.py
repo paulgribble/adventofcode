f4b9b27ca11200c7e8feb9de86fad33e6f8a2a93
Paul Gribble <paul@gribblelab.org>
2019-12-06 08:25:39 -0500

    day06

--- 2019/day06.py
+++ 2019/day06.py
@@ -1,7 +1,6 @@
-
+# Part 1
 orbits = {}
 #f = open('day06_input_test1.txt')
-#f = open('day06_input_test2.txt')
 f = open('day06_input.txt')
 for l in f:
 	o = l.strip('\n').split(')')
@@ -21,4 +20,35 @@ for k in orbits.keys():
 
 print("Part 1: answer is {:d}".format(count))
 
+# Part 2
+# parts adapted from https://pastebin.com/xnj0H31V
 
+data = [l.strip().split(')') for l in open('day06_input.txt').readlines()]
+
+def makegraph(root, data, graph=None):
+	if (graph==None):
+		graph = {root:[]}
+		for k in data:
+			graph[k[1]] = []
+	for c,o in data:
+		if (c==root):
+			graph[o] = graph[c] + [o]
+			makegraph(o, data, graph)
+	return graph
+
+g = makegraph('COM',data)
+
+count = 0
+for o in g['YOU'][::-1][1:]:
+	if o not in g['SAN']:
+		count = count + 1
+	else:
+		break
+
+for o in g['SAN'][::-1][1:]:
+	if o not in g['YOU']:
+		count = count + 1
+	else:
+		break
+
+print("Part 2: answer is {:d}".format(count))
