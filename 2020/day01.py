0db612c04d999c9f4db16ada9e3c7416ef811a14
Paul Gribble <paulgribble@mac.com>
2020-12-01 13:30:50 -0500

    cleanup

--- 2020/day01.py
+++ 2020/day01.py
@@ -2,14 +2,14 @@ with open("day01_input.txt") as f:
 	E = [int(x) for x in f]
 
 n = len(E)
-done = False
 
+done = False
 i = 0
-while ((not done) and (i<n)):
+while (not done and i<n):
 	j = 0
 	while (not done and j<n):
 		if ((E[i]+E[j])==2020):
-			print("Part 1: {:d}*{:d} = {:d}".format(E[i],E[j],E[i]*E[j]))
+			print("Part 1: {:d}".format(E[i]*E[j]))
 			done = True
 		j = j + 1
 	i = i + 1
@@ -22,12 +22,9 @@ while (not done and i<n):
 		k = 0
 		while (not done and k<n):
 			if ((E[i]+E[j]+E[k])==2020):
-				print("Part 2: {:d}*{:d}*{:d} = {:d}".format(E[i],E[j],E[k],E[i]*E[j]*E[k]))
+				print("Part 2: {:d}".format(E[i]*E[j]*E[k]))
 				done = True
 			k = k + 1
 		j = j + 1
 	i = i + 1
-
-
-
 
