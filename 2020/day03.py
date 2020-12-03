f4659583d23788b111dcea72b8215f5d1d62a859
Paul Gribble <paulgribble@mac.com>
2020-12-03 06:57:18 -0500

    day03

---
+++ 2020/day03.py
@@ -0,0 +1,24 @@
+with open("day03_input.txt") as f:
+	grid= [line.strip('\n') for line in f]
+
+def num_trees(right, down, grid):
+	trees = 0
+	r,c = 0,0
+	rows, cols = len(grid), len(grid[0])
+	while r < len(grid):
+		if grid[r%rows][c%cols] == '#':
+			trees += 1
+		r += down
+		c += right
+	return trees	
+
+print("Part 1: {}".format(num_trees(3,1,grid)))
+
+t1 = num_trees(1,1,grid)
+t2 = num_trees(3,1,grid)
+t3 = num_trees(5,1,grid)
+t4 = num_trees(7,1,grid)
+t5 = num_trees(1,2,grid)
+
+print("Part 2: {}".format(t1*t2*t3*t4*t5))
+
