a984e7f313dd9b44f094bf8d1208fc055abcc7f2
Paul Gribble <pgribble@uwo.ca>
2020-12-09 09:03:03 -0500

    day 09 part 1

--- 2020/day09.py
+++ 2020/day09.py
@@ -1,14 +1,13 @@
-with open("day09_input_test.txt") as f:
+with open("day09_input.txt") as f:
 	code = [int(r.strip('\n')) for r in f]
-
 
 def test(code, i, prev):
 	n1,n2 = i-prev,i-prev
 	valid = False
-	while (not valid) and (n1<prev):
+	while (not valid) and (n1<i):
 		n2 = i-prev
-		while (not valid) and (n2<prev):
-			print("{},{}".format(code[n1]+code[n2]==code[i],n1!=n2))
+		while (not valid) and (n2<i):
+#			print("{},{},{},{},{}".format(i,n1,n2,code[n1]+code[n2]==code[i],n1!=n2))
 			if (code[n1]+code[n2]==code[i]) and (n1!=n2):
 				valid = True
 			else:
@@ -17,12 +16,14 @@ def test(code, i, prev):
 			n1 +=1
 	return valid,i,n1,n2
 
-i = 5
-prev = 5
+i = 25
+prev = 25
 valid = True
 while valid:
 	valid,ii,n1,n2 = test(code, i, prev)
 	if valid:
 		i += 1
+
+print("Part 1: {}".format(code[i]))
 
 
