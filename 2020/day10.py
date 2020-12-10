299f4ba9f7b5384f68cb83ce064ac2faa8809ce7
Paul Gribble <paulgribble@mac.com>
2020-12-10 08:18:15 -0500

    day10

--- 2020/day10.py
+++ 2020/day10.py
@@ -1,4 +1,4 @@
-with open("day10_input_test.txt") as f:
+with open("day10_input.txt") as f:
 	A = [int(r.strip('\n')) for r in f]
 
 A.sort()
@@ -47,4 +47,30 @@ def ways(i,AA,W):
 	return W[i]
 
 print("Part 2: {}".format(ways(0,AA,W)))
+
+# saw this one on reddit
+
+from collections import defaultdict
+file = open('day10_input.txt', 'r')
+jolts = [0]
+highest = 0
+
+for line in file:
+    line = line.strip("\n")
+    jolts.append(int(line))
+    highest = max(highest, int(line))
+
+jolts.append(highest + 3)
+
+jolts = sorted(jolts)
+
+ways = defaultdict(lambda:0)
+ways[0] = 1
+
+for i in range(1, len(jolts)):
+    ways[jolts[i]] = ways[jolts[i]-1] + ways[jolts[i]-2] + ways[jolts[i]-3]
+
+print("Part 2: {}".format(ways[jolts[-1]]))
+
+
 
