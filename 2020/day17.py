546c3559a8f460ba37d3ca321e3acf8c48ef1983
Paul Gribble <paulgribble@mac.com>
2020-12-17 13:15:50 -0500

    day17 part 1

--- 2020/day17.py
+++ 2020/day17.py
@@ -1,47 +1,51 @@
-with open("day17_input_test.txt") as f:
+from collections import defaultdict
+
+with open("day17_input.txt") as f:
 	inp = [r.strip('\n') for r in f]
 
 nr,nc = len(inp), len(inp[0])
-x1,x2 = 0,nr-1
-y1,y2 = 0,nc-1
-z1,z2 = 0,0
 
+G = defaultdict(bool) # defaultfict returns False if key doesn't exist
+for ix in range(0,nr):
+	for iy in range(0,nc):
+		G[(ix,iy,0)] = True if inp[ix][iy]=='#' else False
 
-o = (10,10,10)
-import numpy as np
-G = np.zeros((20,20,20))
-
-for ix in range(x1,x2+1):
-	for iy in range(y1,y2+1):
-		for iz in range(z1,z2+1):
-			G[ix+o[0],iy+o[1],iz+o[2]] = 0 if inp[ix][iy]=='.' else 1
-
-def nearactive(G,o,p):
-	x,y,z = p
-	ix,iy,iz = o[0]+x, o[1]+y, o[2]+z
-	n = sum(sum(sum(G[ix-1:ix+2,iy-1:iy+2,iz-1:iz+2] == 1)))
-	if G[ix,iy,iz]==1:
-		n = n - 1
-	return n
-
-
-
-
-mm = (x1,x2,y1,y2,z1,z2)
-
-G = {}
-for ix in range(x1,x2+1):
-	for iy in range(y1,y2+1):
-		G[(ix,iy,0)] = inp[ix][iy]
-
-G2 = G.copy() # shallow copy is ok here since values are just character strings
-
-
-def nearactive(G,x,y,z):
+def numnear(x,y,z):
 	n = 0
-	
+	for ix in range(x-1,x+2):
+		for iy in range(y-1,y+2):
+			for iz in range(z-1,z+2):
+				if G[(ix,iy,iz)] and (ix,iy,iz)!=(x,y,z):
+					n += 1
 	return n
 
+def Grange(G):
+	tmp = list(G.keys())
+	x1,x2,y1,y2,z1,z2 = 0,0,0,0,0,0
+	for g in tmp:
+		x1,x2 = min(x1,g[0]), max(x2,g[0])
+		y1,y2 = min(y1,g[1]), max(y2,g[1])
+		z1,z2 = min(z1,g[2]), max(z2,g[2])
+	return ((x1,x2),(y1,y2),(z1,z2))
+
+nc = 6
+for i in range(nc):
+	G2 = G.copy() # shallow copy is ok since values are scalars
+	xx,yy,zz = Grange(G)
+	for ix in range(xx[0]-1,xx[1]+2):
+		for iy in range(yy[0]-1,yy[1]+2):
+			for iz in range(zz[0]-1,zz[1]+2):
+				if (G[(ix,iy,iz)]==True) and (numnear(ix,iy,iz)==2 or numnear(ix,iy,iz)==3):
+					pass
+				else:
+					G2[(ix,iy,iz)] = False
+				if (G[(ix,iy,iz)]==False) and (numnear(ix,iy,iz)==3):
+					G2[(ix,iy,iz)] = True
+				else:
+					pass
+	G = G2.copy()
+
+print("Part 1: {}".format(sum(G.values())))
 
 
 
