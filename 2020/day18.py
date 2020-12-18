9de5b6b454797ba6585aa5a1bf7b993bdb2b620f
Paul Gribble <paulgribble@mac.com>
2020-12-18 13:26:52 -0500

    despise parsers

--- 2020/day18.py
+++ 2020/day18.py
@@ -18,6 +18,41 @@ def mathme(val1,op,val2):
 
 
 def parse(e):
+	if e.find(' ')==-1: # must be a single integer
+		print("{}: returning {}".format(e,int(e)))
+		return int(e)
+	elif e[0] in '0123456789': # more than one term is left
+		i = e.find(' ')
+		val1 = int(e[:i])
+		op = e[i+1]
+		print("{}: returning {} {} {}".format(e,val1,op,e[i+3:]))
+		val2 = parse(e[i+3:])
+		r = mathme(val1,op,val2)
+		return r
+	elif e[0]=='(':
+		depth = 1
+		i = 1
+		while depth>0 and i<len(e):
+			if e[i]=='(':
+				depth += 1
+			elif e[i]==')':
+				depth -= 1
+			i += 1
+		i -= 1
+		print("{}: returning parse({})".format(e,e[1:i]))
+		return parse(e[1:])
+
+
+
+
+
+
+
+
+
+
+
+def parse(e):
 	print('e: {}'.format(e))
 	if e=='':
 		print("{}: returning 0".format(e))
