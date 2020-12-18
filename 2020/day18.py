cb82473302cb5f0bf4f0922b6de2fb4374f5fa27
Paul Gribble <paulgribble@mac.com>
2020-12-18 12:24:06 -0500

    i hate parsers

--- 2020/day18.py
+++ 2020/day18.py
@@ -4,18 +4,35 @@ with open("day18_input.txt") as f:
 e = "1 + (2 * 3) + (4 * (5 + 6))"
 
 
+def mathme(val1,op,val2):
+	val1,val2 = int(val1), int(val2)
+	print("{} {} {}".format(val1,op,val2))
+	if op=='+':
+		return val1 + val2
+	elif op=='-':
+		return val1 - val2
+	elif op=='*':
+		return val1 * val2
+	elif op=='/':
+		return val1 / val2
+
+
 def parse(e):
 	print('e: {}'.format(e))
 	if e=='':
-		print('empty: 0')
+		print("returning 0")
 		return 0
 	elif e.find(' ')==-1:
-		print('no space: {}'.format(int(e)))
+		print("returning {}".format(int(e)))
 		return int(e)
 	elif e[0] in '0123456789':
 		i = e.find(' ')
 		val1 = int(e[:i])
-		print('val1: {}'.format(val1))
+		op = e[i+1]
+		val2 = parse(e[i+3:])
+		r = mathme(val1,op,val2)
+		print("returning {}".format(r))
+		return r
 	elif e[0]=='(':
 		depth = 1
 		i = 1
@@ -25,18 +42,16 @@ def parse(e):
 			elif e[i]==')':
 				depth -= 1
 			i += 1
-		val1 = parse(e[:i])
-		print('(: val1: {}'.format(val1))
-	op = e[i+1]
-	val2 = parse(e[i+3:])
-	if op=='+':
-		return val1 + val2
-	elif op=='-':
-		return val1 - val2
-	elif op=='*':
-		return val1 * val2
-	elif op=='/':
-		return val1 / val2
+		val1 = parse(e[1:i-1])
+		if i < len(e):
+			op = e[i+1]
+			val2 = e[i+3:]
+			r = mathme(val1,op,val2)
+			print("returning {}".format(r))
+			return r
+		else:
+			print("returning {}".formate(val1))
+			return val1
 
 
 
