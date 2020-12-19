69e96676fbde42bf18b844edad6a64b1db47f0f9
Paul Gribble <paulgribble@mac.com>
2020-12-19 10:12:54 -0500

    gaveup

--- 2020/day19.py
+++ 2020/day19.py
@@ -2,18 +2,41 @@ with open("day19_input_test2.txt") as f:
 	inp = [r.strip('\n') for r in f]
 
 rules = {}
+messages = []
 
+
+
+
+
+
+readrules = True
 for l in inp:
-	k,tmp = l.split(': ')
-	k = int(k)
-	if tmp[0] == '"':
-		rules[k] = tmp.replace('"','')
-	else:
-		if tmp.find(' | ')==-1:
-			rules[k] = list(map(int,tmp.split(' ')))
+	if l=='' and readrules:
+		readrules = False
+	if readrules:
+		v,k = l.split(': ')
+		if k[0] == '"':
+			if k[1]=='a':
+				a_r = v
+			elif k[1]=='b':
+				b_r = v
+		elif l.find(' | ')==-1:
+			k = k.replace(' ','')
+			rules[k] = v
 		else:
-			v1,v2 = tmp.split(' | ')
-			rules[k] = [list(map(int,v1.split(' '))), list(map(int,v2.split(' ')))]
+			a,b = k.split(' | ')
+			a,b = a.replace(' ',''), b.replace(' ','')
+			rules[a] = v
+			rules[b] = v
+	else:
+		if l != '':
+			messages.append(l)
+
+
+mi = messages[0]
+m = mi.replace('a',a_r).replace('b',b_r)
+
+def valid(m):
 
 
 
