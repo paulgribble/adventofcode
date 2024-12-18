0502d6f5e2d44c57db6ab9a8bd16d2a0277aa1c5
Paul Gribble <pgribble@uwo.ca>
2024-12-17 19:46:27 -0500

    fix day3

--- 2024/day03.py
+++ 2024/day03.py
@@ -4,13 +4,13 @@ with open("day03_input.txt") as f:
     data = f.read()
 
 r = 0
-for i,j,k in re.findall("(mul\(([0-9]{1,3}),([0-9]{1,3})\))", data):
+for i,j,k in re.findall("(mul\\(([0-9]{1,3}),([0-9]{1,3})\\))", data):
     r += int(j) * int(k)
 print(f"Part 1: {r}")
 
 r = 0
 enabled = True
-for i,j,k in re.findall("(mul\((\d+),(\d+)\)|do\(\)|don't\(\))", data):
+for i,j,k in re.findall("(mul\\(([0-9]{1,3}),([0-9]{1,3})\\)|do\\(\\)|don't\\(\\))", data):
     if (enabled and i[0:3]=='mul'):
         r += int(j) * int(k)
     elif (i[0:3]=='don'):
