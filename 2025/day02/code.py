3d7ca54f8810d866ac916e583069fb87ed8da7a5
Paul Gribble <pgribble@uwo.ca>
2025-12-04 09:30:06 -0500

    day02

--- 2025/day02/code.py
+++ 2025/day02/code.py
@@ -14,3 +14,35 @@ for idi in IDs:
 				thesum += i
 print(f"day1: {thesum}")
 
+
+def isInvalid(i):
+	ii = str(i)
+	l = len(ii)
+	segmax = l//2
+	for j in range(1,segmax+1):
+		if ((l%j)==0):
+			pool = set()
+			k=0
+			while (((k+1)*j)<=l):
+				i1,i2 = k*j, (k+1)*j
+#				print(f"{i1},{i2}")
+				pool.add(ii[i1:i2])
+				k += 1
+#			print(pool)
+			if len(pool)==1:
+				return True
+	return False
+
+
+thesum = 0
+for idi in IDs:
+	i1,i2 = idi.split('-')
+	i1,i2 = int(i1), int(i2)
+	for i in range(i1,i2+1):
+		if isInvalid(i):
+#			print(f"invalid: {i}")
+			thesum += i
+
+print(f"day2: {thesum}")
+
+
