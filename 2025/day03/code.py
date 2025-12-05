78cff07852744ff8c5049b8b53eea4a4d0eb9fe4
Paul Gribble <pgribble@uwo.ca>
2025-12-04 19:49:37 -0500

    part 2

--- 2025/day03/code.py
+++ 2025/day03/code.py
@@ -1,4 +1,4 @@
-with open("input2") as f:
+with open("input") as f:
 	BATT = [line.strip() for line in f.readlines()]
 
 sum_voltage = 0
@@ -18,6 +18,20 @@ print(f"part 1: {sum_voltage}")
 sum_voltage = 0
 for B in BATT:
 	l = len(B)
+	joltage = []
+	i1,i2 = 0,l-12+1
+	while (len(joltage)<12):
+#		print(f"i1,i2: {i1},{i2}")
+		imaxv = int(B[i1])
+		imaxi = i1
+		for i in range(i1,i2):
+			if (int(B[i])>imaxv):
+				imaxv = int(B[i])
+				imaxi = i
+		joltage.append(imaxv)
+		i1=imaxi+1
+		i2=i2+1
+	sum_voltage += int("".join([str(x) for x in joltage]))
 
 print(f"part 2: {sum_voltage}")
 
