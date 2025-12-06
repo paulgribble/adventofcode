7d3e667245e0d966d5f0be4aac66daf8ba879bdd
Paul Gribble <pgribble@uwo.ca>
2025-12-06 07:45:18 -0500

    part 2

--- 2025/day05/code.py
+++ 2025/day05/code.py
@@ -26,9 +26,37 @@ print(f"part 1: {num_fresh}")
 
 
 
+# part 2 with help from reddit
 
-fresh = 0
+datafile = open('input', "r", encoding="UTF-8").read().strip().splitlines()
+empty_line = datafile.index("")
+s_ranges = datafile[:empty_line]
 
+def parse_range(rng):
+    a, b = map(int, rng.split('-'))
+    return a, b
+
+ranges = sorted(parse_range(r) for r in s_ranges)
+
+def merge_ranges(a,b):
+    start = min(a[0], b[0])
+    end = max(a[1], b[1])
+    return start, end
+
+def overlaps(a,b):
+    return (b[1] >= a[0] and b[0] <= a[1])
+
+final = []
+current = ranges[0]
+for range in ranges[1:]:
+    if overlaps(current, range):
+        current = merge_ranges(current, range)
+    else:
+        final.append(current)
+        current = range
+final.append(current)
+
+fresh = sum([b-a+1 for a, b in final])
 
 print(f"part 2: {fresh}")
 
